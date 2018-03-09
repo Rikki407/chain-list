3d6ad3bdfbb766391cf4e01ac01b0a1e26901dd2
Rikki407 <rishablamba407@gmail.com>
2018-03-09 15:44:48 +0530

    v2

---
+++ contracts/ChainList.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.18;
+
+contract ChainList{
+    address seller;
+    string name;
+    string description;
+    uint256 price;
+
+    //sell an article
+    function sellArticle(string _name,string _description,uint256 _price) public{
+        seller = msg.sender;
+        name = _name;
+        description = _description;
+        price = _price;
+    }
+
+    //get an article
+    function getArticle() view returns(address,string,string,uint256){
+        return (seller,name,description,price);
+    }
+}
