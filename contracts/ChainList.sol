1433477de81793f528e88ffe64be8a7551158eb3
Rikki407 <rishablamba407@gmail.com>
2018-03-11 13:04:52 +0530

    list v1

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -1,53 +1,82 @@
 pragma solidity ^0.4.18;
 
-contract ChainList{
-    //state variables
+contract ChainList {
+  //custom types
+  struct Article{
+    uint id;
     address seller;
     address buyer;
     string name;
     string description;
     uint256 price;
+  }
+  // state variables
+  mapping(uint=>Article) public articles;
+  uint articleCounter;
 
-    // events
-    event LogSellArticle(
-        address indexed _seller,
-        string _name,
-        uint256 price
+
+  // events
+  event LogSellArticle(
+    uint indexed _id,
+    address indexed _seller,
+    string _name,
+    uint256 _price
+  );
+  event LogBuyArticle(
+    address indexed _seller,
+    address indexed _buyer,
+    string _name,
+    uint256 _price
+  );
+
+  // sell an article
+  function sellArticle(string _name, string _description, uint256 _price) public {
+    //a new article
+    articleCounter++;
+   //Storing this article
+    articles[articleCounter] = Article(
+      articleCounter,
+      msg.sender,
+      0x0,
+      _name,
+      _description,
+      _price
     );
-    event LogBuyArticle(
-        address indexed _seller,
-        address indexed _buyer,
-        string _name,
-        uint256 price
-    );
+    LogSellArticle(articleCounter,seller, name, price);
+  }
 
-    //sell an article
-    function sellArticle(string _name,string _description,uint256 _price) public{
-        seller = msg.sender;
-        name = _name;
-        description = _description;
-        price = _price;
+  // get an article
+  function getArticle() public view returns (
+    address _seller,
+    address _buyer,
+    string _name,
+    string _description,
+    uint256 _price
+  ) {
+      return(seller, buyer, name, description, price);
+  }
 
-        LogSellArticle(seller,name,price);
-    }
+  // buy an article
+  function buyArticle() payable public {
+    // we check whether there is an article for sale
+    require(seller != 0x0);
 
-    //get an article
-    function getArticle() public view returns(address,address,string,string,uint256){
-        return (seller,buyer,name,description,price);
-    }
+    // we check that the article has not been sold yet
+    require(buyer == 0x0);
 
-    //buy an article
-    function buyArticle() payable public{
-        //we check if there is an article for sale
-        require(seller!=0x0);
-        //we check that article has not already been sold
-        require(buyer == 0x0);
-        require(msg.sender!=seller);
-        require(msg.value == price);
-        buyer = msg.sender;
-        seller.transfer(msg.value);
-         LogBuyArticle(seller,buyer,name,price);
+    // we don't allow the seller to buy his own article
+    require(msg.sender != seller);
 
-        
-    }
+    // we check that the value sent corresponds to the price of the article
+    require(msg.value == price);
+
+    // keep buyer's information
+    buyer = msg.sender;
+
+    // the buyer can pay the seller
+    seller.transfer(msg.value);
+
+    // trigger the event
+    LogBuyArticle(seller, buyer, name, price);
+  }
 }
